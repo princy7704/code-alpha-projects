fd6d3eb0be89d7cf63da4e47e730ae2d29ec810b
princy7704 <gaddamprince77@gmail.com>
2026-05-25 17:07:41 +0530

    Delete management directory

--- management/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import *
-
-admin.site.register(MenuItem)
-admin.site.register(Table)
-admin.site.register(Order)
-admin.site.register(Reservation)
-admin.site.register(Inventory)
